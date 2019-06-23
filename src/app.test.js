da733e7a1118f9add6fb7f80592f5e87030aa9e8
Zack Jones <zackjones11@gmail.com>
2019-06-23 15:35:07 +0100

    updated app.test

--- src/app.test.js
+++ src/app.test.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './app'
+import ConnectedComponent from './app'
 
 window.alert = () => {}
 
+const App = ConnectedComponent.WrappedComponent;
+
 it('renders without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<App />, div)
+    const fetchVenues = jest.fn()
+    const venues = []
+
+    ReactDOM.render(<App fetchVenues={fetchVenues} venues={venues}/>, div)
     ReactDOM.unmountComponentAtNode(div)
 })
