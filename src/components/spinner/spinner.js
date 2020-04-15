6db6c0de8b6fd90e8d3109932d144f71abc1b767
Zack Jones <zackjones11@gmail.com>
2020-04-15 22:12:59 +0200

    added absolutePosition prop for spinner component

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -2,8 +2,13 @@ import React from "react";
 import classNames from "classnames";
 import styles from "./spinner.module.css";
 
-const Spinner = ({ style }) => (
-  <div className={classNames([styles.spinner, styles.spinnerCenter])} style={style}>
+const Spinner = ({ style, absolutePosition = false }) => (
+  <div
+    className={classNames([styles.spinner, styles.spinnerCenter], {
+      [`${styles.spinnerAbsolute}`]: absolutePosition,
+    })}
+    style={style}
+  >
     <div className={styles.spinnerInner}></div>
     <div className={styles.spinnerInner}></div>
     <div className={styles.spinnerInner}></div>
