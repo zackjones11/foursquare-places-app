1816ed3e83acb42dd4bf5d5de204230bacb63d27
Zack Jones <zackjones11@gmail.com>
2020-04-15 20:09:18 +0200

    refactored venue list item to be functional component

--- src/components/venue-list-item/venue-list-item.js
+++ src/components/venue-list-item/venue-list-item.js
@@ -1,15 +1,13 @@
 import React from "react";
 import styles from "./venue-list-item.module.css";
 
-const VenueListItem = ({ venue, onClick }) => {
-  const address = venue.location.formattedAddress.join(", ");
-
-  return (
-    <li data-id={venue.id} className={styles.listItem} onClick={onClick}>
-      <div className={styles.name}>{venue.name}</div>
-      <div className={styles.address}>{address}</div>
-    </li>
-  );
-};
+const VenueListItem = ({ venue, onClick }) => (
+  <li className={styles.listItem} onClick={() => onClick(venue)}>
+    <div className={styles.name}>{venue.name}</div>
+    <div className={styles.address}>
+      {venue.location.formattedAddress.join(", ")}
+    </div>
+  </li>
+);
 
 export default VenueListItem;
