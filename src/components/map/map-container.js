b65a5230e5b4d06c9f177bf171b539b1bfff4d8b
Zack Jones <zackjones11@gmail.com>
2020-04-15 22:13:49 +0200

    migrated Google Maps api to @react-google-maps/api

--- src/components/map/map-container.js
+++ src/components/map/map-container.js
@@ -1,50 +1,47 @@
 import React from "react";
 import { connect } from "react-redux";
 import Spinner from "../spinner/spinner";
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { GOOGLE_MAPS_API_KEY } from "../../constants/google-maps-api";
 
-const MapContainer = ({ google, usersLocation, venueLocation }) => {
+const MapContainer = ({ usersLocation, venueLocation }) => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: GOOGLE_MAPS_API_KEY,
+  });
+
+  if (loadError) {
+    alert("Maps cannot be loaded right now.");
+    return null;
+  }
+
   const location = venueLocation.lat ? venueLocation : usersLocation;
+
   return (
-    <Map
-      google={google}
-      zoom={venueLocation.lat ? 16 : 14}
-      mapTypeControl={false}
-      fullscreenControl={false}
-      style={{
-        width: "100%",
-        height: "100%",
-      }}
-      initialCenter={location}
-      center={location}
-    >
-      <Marker position={location} />
-    </Map>
+    <>
+      {isLoaded ? (
+        <GoogleMap
+          zoom={venueLocation.lat ? 16 : 14}
+          mapContainerStyle={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+          }}
+          initialCenter={location}
+          center={location}
+          options={{ mapTypeControl: false, fullscreenControl: false }}
+        >
+          <Marker position={location} />
+        </GoogleMap>
+      ) : (
+        <Spinner absolutePosition />
+      )}
+    </>
   );
 };
-
-const LoadingContainer = () => (
-  <Spinner
-    style={{
-      position: "absolute",
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      margin: "auto",
-    }}
-  />
-);
 
 const mapStateToProps = (state) => ({
   usersLocation: state.geolocation.usersLocation,
   venueLocation: state.venues.selectedVenue.location,
 });
 
-export default connect(mapStateToProps)(
-  GoogleApiWrapper({
-    apiKey: GOOGLE_MAPS_API_KEY,
-    LoadingContainer,
-  })(MapContainer)
-);
+export default connect(mapStateToProps)(MapContainer);
