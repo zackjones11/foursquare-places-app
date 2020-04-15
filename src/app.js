607decdc7de26b9a600d121b2460d361e14420b2
Zack Jones <zackjones11@gmail.com>
2020-04-15 21:23:47 +0200

    replaces componentDidMount with useEffect

--- src/app.js
+++ src/app.js
@@ -1,32 +1,30 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-
 import SearchBar from "./components/search-bar/search-bar";
 import MapContainer from "./components/map/map-container";
 import VenueList from "./components/venue-list/venue-list";
-
 import * as geolocation from "./redux/geolocation-redux";
 
 import styles from "./app.module.css";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.fetchGeoLocation();
-  }
+const App = (props) => {
+  const { hasUsersLocation, fetchGeoLocation, showingVenueList } = props;
 
-  render() {
-    return (
-      <div className={styles.container}>
-        <div>{this.props.hasUsersLocation && <MapContainer />}</div>
+  useEffect(() => {
+    fetchGeoLocation();
+  }, [fetchGeoLocation]);
 
-        <div className={styles.sidebar}>
-          <SearchBar />
-          {this.props.showingVenueList && <VenueList />}
-        </div>
+  return (
+    <div className={styles.container}>
+      <div>{hasUsersLocation && <MapContainer />}</div>
+
+      <div className={styles.sidebar}>
+        <SearchBar />
+        {showingVenueList && <VenueList />}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   venues: state.venues.venues,
